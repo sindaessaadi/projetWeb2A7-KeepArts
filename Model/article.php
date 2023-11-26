848bbe0b4bc0a4c10a5b082ac142a9434873542d
fedi-nabli <fedinabli@gmail.com>
2023-11-26 12:17:10 +0100

    Article model goes brrrr

--- Model/article.php
+++ Model/article.php
@@ -4,19 +4,18 @@ class Article
 
     private ?int $id = null;
     private ?string $nom = null;
-    private ?string $artiste = null;
+    private ?int $artiste = null;
     private ?string $type = null;
-    private ?date $date = null;
+    private $date = null;
     private ?bool $status = null;
-}
-    
 
-    public function __construct( $n, $a, $t, $d, $s)
+
+    public function __construct($n, $a, $t, $d, $s)
     {
         $this->nom = $n;
         $this->artiste = $a;
         $this->type = $t;
-        $this->date = $d;
+        $this->date = new DateTime($d);
         $this->status = $s;
     }
 
@@ -26,37 +25,29 @@ class Article
         
     }
 
-
-    public function getnom_article()
+    public function getArticleName()
     {
         return $this->nom;
     }
 
-    @return  self
-
-    public function getdate()
+    public function getDate()
     {
         return $this->date;
         
     }
-    @return  self
-
 
     public function getArtiste(): string
     {
         return $this->artiste;
-        }
-        @return  self
+    }
     
     public function getType(): string
     {
         return $this->type;
-        }
-        @return  self
+    }
     
     public function getStatus(): bool
     {
         return $this->status;
     }
-
-
+}
